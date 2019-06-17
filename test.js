8be78609a07798ab9bb99f1b209e166f727a8df0
monkey999por <yururi1122@gmail.com>
2019-06-17 23:45:21 +0900

    test.jsをマスターにコミット

--- test.js
+++ test.js
@@ -6,6 +6,15 @@ let sub_array = ['a', 'b', 'c'];
 let str = 'test';
 str += sub_array[1];
 console.log(str);
+//masterで競合させてみる
+//Gitのテスト main //こっちではサブをいろいろ直すよ～
+let main = 'main_value';
+//キョウオウ
+let sub_array = ['a', 'b', 'c'];
+let str = 'test';
+str += sub_array[1];
+console.log(str);
+
 
 
 
